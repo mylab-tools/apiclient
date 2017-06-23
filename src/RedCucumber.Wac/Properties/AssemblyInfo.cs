d3fd0eec4ea9a4b2e3dc387ac41f9261f8a1ac1a
ozzy <ozzy@triasoft.com>
2017-06-23 15:15:45 +0300

    Add nuget projectrs

--- src/RedCucumber.Wac/Properties/AssemblyInfo.cs
+++ src/RedCucumber.Wac/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RedCucumber.Wac")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Andrey Osintsev 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.2306")]
+[assembly: AssemblyFileVersion("1.0.0.2306")]
 [assembly: InternalsVisibleTo("NetFramework.UnitTests")]
