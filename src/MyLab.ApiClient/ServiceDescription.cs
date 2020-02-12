2acaaa7facdafbabd773ba4b8973736f0dc103c9
Andrey O <ozzy@triasoft.com>
2020-02-12 14:47:45 +0300

    Add ServiceDescription empty

---
+++ src/MyLab.ApiClient/ServiceDescription.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLab.ApiClient
+{
+    class ServiceDescription
+    {
+        public string BaseUrl { get; private set; }
+
+
+
+        public static ServiceDescription Create<TContract>()
+        {
+
+        }
+    }
+}
