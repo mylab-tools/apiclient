16b0afc85597914f53a92bf984962ed9bf00f136
Andrey O <ozzy@triasoft.com>
2020-02-21 02:18:25 +0300

    Support single HttpClient for ApiClient

--- src/MyLab.ApiClient/HttpClientProvider.cs
+++ src/MyLab.ApiClient/HttpClientProvider.cs
@@ -4,39 +4,60 @@ using Microsoft.Extensions.Options;
 
 namespace MyLab.ApiClient
 {
+    /// <summary>
+    /// Specifies <see cref="HttpClient"/> provider
+    /// </summary>
     public interface IHttpClientProvider
     {
+        /// <summary>
+        /// Provides http client
+        /// </summary>
         HttpClient Provide();
     }
 
-    class DefaultHttpClientProvider : IHttpClientProvider
+    public class FactoryHttpClientProvider : IHttpClientProvider
     {
         private readonly IHttpClientFactory _httpClientFactory;
         private readonly string _serviceName;
 
         /// <summary>
-        /// Initializes a new instance of <see cref="DefaultHttpClientProvider"/>
+        /// Initializes a new instance of <see cref="FactoryHttpClientProvider"/>
         /// </summary>
-        public DefaultHttpClientProvider(IHttpClientFactory httpClientFactory)
+        public FactoryHttpClientProvider(IHttpClientFactory httpClientFactory)
             :this(httpClientFactory, Options.DefaultName)
         {
             
         }
         /// <summary>
-        /// Initializes a new instance of <see cref="DefaultHttpClientProvider"/>
+        /// Initializes a new instance of <see cref="FactoryHttpClientProvider"/>
         /// </summary>
-        public DefaultHttpClientProvider(IHttpClientFactory httpClientFactory, string serviceName)
+        public FactoryHttpClientProvider(IHttpClientFactory httpClientFactory, string serviceName)
         {
             _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
             _serviceName = serviceName;
         }
 
-        /// <summary>
-        /// Provides http client factory
-        /// </summary>
         public HttpClient Provide()
         {
             return _httpClientFactory.CreateClient(_serviceName);
+        }
+    }
+
+    public class SingleHttpClientProvider : IHttpClientProvider
+    {
+        private readonly HttpClient _client;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="SingleHttpClientProvider"/>
+        /// </summary>
+        public SingleHttpClientProvider(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public HttpClient Provide()
+        {
+            return _client;
         }
     }
 }
