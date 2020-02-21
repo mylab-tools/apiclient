7b5f6a6af6de6d408828e6907cdf940a821e4ebd
Andrey O <ozzy@triasoft.com>
2020-02-21 14:44:01 +0300

    Add Markup validator

---
+++ src/UnitTests/MarkupValidatorBehavior.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using MyLab.ApiClient;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace UnitTests
+{
+    public partial class MarkupValidatorBehavior
+    {
+        [Fact]
+        public void ShouldPassValidContract()
+        {
+            //Arrange
+            
+
+            //Act
+            var valRes = MarkupValidator.Validate(typeof(IRightContract));
+            
+            //Assert
+            Assert.Empty(valRes);
+        }
+
+        [Theory]
+        [InlineData(typeof(IContractWithMethodWithWrongUrl))]
+        [InlineData(typeof(IContractWithMethodWithoutAttr))]
+        [InlineData(typeof(IContractWithParameterWithoutAttr))]
+        [InlineData(typeof(IContractWithSeveralContentParams))]
+        [InlineData(typeof(IContractWithWrongUrl))]
+        [InlineData(typeof(IContractWithoutApiAttr))]
+        public void ShouldDetectCriticalWrongContract(Type contractType)
+        {
+            //Arrange
+
+
+            //Act
+            var valRes = MarkupValidator.Validate(contractType).ToArray();
+
+            WriteLog(valRes);
+
+            //Assert
+            Assert.Contains(valRes, iss => iss.Critical);
+        }
+
+        [Theory]
+        [InlineData(typeof(IContractWithEmptyUrl))]
+        [InlineData(typeof(IContractWithMethodWithEmptyUrl))]
+        public void ShouldDetectWarningWrongContract(Type contractType)
+        {
+            //Arrange
+
+
+            //Act
+            var valRes = MarkupValidator.Validate(contractType).ToArray();
+
+            WriteLog(valRes);
+
+            //Assert
+            Assert.True(valRes.All(iss => !iss.Critical));
+        }
+    }
+}
---
+++ src/UnitTests/MarkupValidatorBehavior.stuff.cs
@@ -0,0 +1,102 @@
+﻿using MyLab.ApiClient;
+using Xunit.Abstractions;
+
+namespace UnitTests
+{
+    public partial class MarkupValidatorBehavior
+    {
+        private readonly ITestOutputHelper _output;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="MarkupValidatorBehavior"/>
+        /// </summary>
+        public MarkupValidatorBehavior(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        private void WriteLog(MarkupValidationIssuer[] valRes)
+        {
+            _output.WriteLine("===== Validation Result =====");
+
+            if (valRes.Length == 0)
+            {
+                _output.WriteLine("<empty>");
+            }
+            else
+            {
+                foreach (var i in valRes)
+                {
+                    _output.WriteLine("");
+                    _output.WriteLine(i.Reason + (i.Critical ? " (critical)" : " (warning)"));
+                    _output.WriteLine("\t" + i.ServiceContract.FullName);
+                    if (i.Method != null)
+                        _output.WriteLine("\t\t" + i.Method.Name);
+                    if (i.Parameter != null)
+                        _output.WriteLine("\t\t" + i.Parameter.Name);
+                }
+            }
+        }
+
+        [Api("api")]
+        private interface IRightContract
+        {
+            [Post("test")]
+            void Post([Query] int a1, [Path] int a2, [Header("a3")] int a3, [StringContent] int a4);
+        }
+
+        private interface IContractWithoutApiAttr
+        {
+            [Post("test")]
+            void Post([Query] int a1, [Path] int a2, [Header("a3")] int a3, [StringContent] int a4);
+        }
+
+        [Api("")]
+        private interface IContractWithEmptyUrl
+        {
+            [Post("test")]
+            void Post([Query] int a1, [Path] int a2, [Header("a3")] int a3, [StringContent] int a4);
+        }
+
+        [Api("http://http://")]
+        private interface IContractWithWrongUrl
+        {
+            [Post("test")]
+            void Post([Query] int a1, [Path] int a2, [Header("a3")] int a3, [StringContent] int a4);
+        }
+
+        [Api("api")]
+        private interface IContractWithMethodWithoutAttr
+        {
+            void Post([Query] int a1, [Path] int a2, [Header("a3")] int a3, [StringContent] int a4);
+        }
+
+        [Api("api")]
+        private interface IContractWithMethodWithEmptyUrl
+        {
+            [Post("")]
+            void Post([Query] int a1, [Path] int a2, [Header("a3")] int a3, [StringContent] int a4);
+        }
+
+        [Api("api")]
+        private interface IContractWithMethodWithWrongUrl
+        {
+            [Post("http://http://")]
+            void Post([Query] int a1, [Path] int a2, [Header("a3")] int a3, [StringContent] int a4);
+        }
+
+        [Api("api")]
+        private interface IContractWithParameterWithoutAttr
+        {
+            [Post("test")]
+            void Post(int a1, [Path] int a2, [Header("a3")] int a3, [StringContent] int a4);
+        }
+
+        [Api("api")]
+        private interface IContractWithSeveralContentParams
+        {
+            [Post("test")]
+            void Post([Query] int a1, [JsonContent] int a2, [Header("a3")] int a3, [StringContent] int a4);
+        }
+    }
+}
