6cfd863c7c807dd4dcb3463327d18c3a95ff95e3
Andrey O <ozzy@triasoft.com>
2020-04-25 22:12:03 +0300

    Add comments to options

--- src/MyLab.ApiClient/ApiClientsOptions.cs
+++ src/MyLab.ApiClient/ApiClientsOptions.cs
@@ -2,13 +2,25 @@
 
 namespace MyLab.ApiClient
 {
+    /// <summary>
+    /// Contains api clients infrastructure options
+    /// </summary>
     public class ApiClientsOptions
     {
+        /// <summary>
+        /// List of api connections options
+        /// </summary>
         public Dictionary<string, ApiConnectionOptions> List { get; set; }
     }
 
+    /// <summary>
+    /// Contains api connection options
+    /// </summary>
     public class ApiConnectionOptions
     {
+        /// <summary>
+        /// API base url
+        /// </summary>
         public string Url { get; set; }
     }
 }
