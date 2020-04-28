8949de5a28c6dc14ea85a4f222756b5bc5b87af1
Andrey O <ozzy@triasoft.com>
2020-04-28 15:35:32 +0300

    Revert "Add Test lib"
    This reverts commit 150b5f79286e520a5a9127fe4a7cae1d71533c08.

--- src/IntegrationTests/EmptyResponseHandleBehavior.cs
+++ src/IntegrationTests/EmptyResponseHandleBehavior.cs
@@ -1,4 +1,6 @@
 ﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Logging;
 using MyLab.ApiClient;
 using TestServer;
 using Xunit;
@@ -6,22 +8,31 @@ using Xunit.Abstractions;
 
 namespace IntegrationTests
 {
-    public class EmptyResponseHandleBehavior : MyLab.ApiClient.Test.ApiClientTest<Startup, EmptyResponseHandleBehavior.ITestServer>
+    public class EmptyResponseHandleBehavior : IClassFixture<WebApplicationFactory<Startup>>
     {
-        public EmptyResponseHandleBehavior(ITestOutputHelper output)
-            :base(output)
+        private readonly IHttpClientProvider _clientProvider;
+        private readonly ITestOutputHelper _output;
+
+        public EmptyResponseHandleBehavior(WebApplicationFactory<Startup> webApplicationFactory, ITestOutputHelper output)
         {
+            _clientProvider = new DelegateHttpClientProvider(webApplicationFactory.CreateClient);
+            _output = output;
         }
 
         [Fact]
         public async Task ShouldReturnNullIfStringResponseIsEmpty()
         {
             //Arrange
+            var client = new ApiClient<ITestServer>(_clientProvider);
 
             //Act 
-            var resDet = await TestCall(s => s.GetNullString());
-            
+            var resDet = await client.Call(s => s.GetNullString()).GetDetailed();
+            Log(resDet);
+
             //Assert
+            if (resDet.ResponseContent == string.Empty)
+                _output.WriteLine("String is empty");
+
             Assert.Null(resDet.ResponseContent);
         }
 
@@ -29,10 +40,12 @@ namespace IntegrationTests
         public async Task ShouldReturnNullIfValueResponseIsEmpty()
         {
             //Arrange
-            
+            var client = new ApiClient<ITestServer>(_clientProvider);
+
             //Act 
-            var resDet = await TestCall(s => s.GetNullValue());
-            
+            var resDet = await client.Call(s => s.GetNullValue()).GetDetailed();
+            Log(resDet);
+
             //Assert
             Assert.Equal(default, resDet.ResponseContent);
         }
@@ -41,16 +54,30 @@ namespace IntegrationTests
         public async Task ShouldReturnNullIfArrayResponseIsEmpty()
         {
             //Arrange
-            
+            var client = new ApiClient<ITestServer>(_clientProvider);
+
             //Act 
-            var resDet = await TestCall(s => s.GetNullArray());
+            var resDet = await client.Call(s => s.GetNullArray()).GetDetailed();
+            Log(resDet);
 
             //Assert
+            if(resDet.ResponseContent != null)
+                _output.WriteLine("Array length: " + resDet.ResponseContent.Length);
+
             Assert.Null(resDet.ResponseContent);
         }
 
+        private void Log<T>(CallDetails<T> resDet)
+        {
+            _output.WriteLine("Request:");
+            _output.WriteLine(resDet.RequestDump);
+            _output.WriteLine("Response:");
+            _output.WriteLine(resDet.ResponseDump);
+            
+        }
+
         [Api("echo/empty")]
-        public interface ITestServer
+        interface ITestServer
         {
             [Get]
             Task<string> GetNullString();
