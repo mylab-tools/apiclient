7de6e0806a2e88f0a9608c0825f21daf1eed9faa
Andrey O <ozzy@triasoft.com>
2020-11-20 17:02:42 +0300

    Add test about proxy-client integration in singleton

---
+++ src/UnitTests/ApiProxyBehavior.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using MyLab.ApiClient;
+using Xunit;
+
+namespace UnitTests
+{
+    public class ApiProxyBehavior
+    {
+        [Fact]
+        public void ShouldBeProvidedInSingleton()
+        {
+            //Arrange
+
+            ServiceProvider services = new ServiceCollection()
+                .AddApiClients(
+                    r => r.RegisterContract<ITestApi>(),
+                    new ApiClientsOptions
+                    {
+                        List = new Dictionary<string, ApiConnectionOptions>
+                        {
+                            {"test-key", new ApiConnectionOptions()}
+                        }
+                    })
+                .AddSingleton<SingletonService>()
+                .BuildServiceProvider();
+
+            //Act
+            var ss = services.GetService<SingletonService>();
+
+            //Assert
+            Assert.NotNull(ss);
+            Assert.NotNull(ss.TestApi);
+        }
+
+        [Api("bla", Key = "test-key")]
+        interface ITestApi
+        {
+            [Get]
+            Task Foo();
+        }
+
+        class SingletonService
+        {
+            public ITestApi TestApi { get; }
+
+            public SingletonService(ITestApi testApi)
+            {
+                TestApi = testApi;
+            }
+        }
+    }
+}
