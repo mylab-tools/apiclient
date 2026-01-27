b78d611a5753ca23d18ae2cd698604ec1793aff1
Andrey O. <ozzy.ext@gmail.com>
2026-01-27 15:44:04 +0300

    Full obsolete ApiAttribute

--- src/MyLab.ApiClient/Contracts/Attributes/ForContract/ApiAttribute.cs
+++ src/MyLab.ApiClient/Contracts/Attributes/ForContract/ApiAttribute.cs
@@ -9,4 +9,21 @@ namespace MyLab.ApiClient.Contracts.Attributes.ForContract;
 [AttributeUsage(AttributeTargets.Interface)]
 public class ApiAttribute : Attribute
 {
+    /// <summary>
+    /// Base service URL
+    /// </summary>
+    public string? Url { get; }
+
+    /// <summary>
+    /// Determine key to bind with configuration
+    /// </summary>
+    public string? Key { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="ApiAttribute"/>
+    /// </summary>
+    public ApiAttribute(string? url = null)
+    {
+        Url = url;
+    }
 }
