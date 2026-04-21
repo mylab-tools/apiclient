2e4ad2e7f050a0761be60315d51b60dd804a174a
Andrey O. <ozzy.ext@gmail.com>
2026-04-21 22:29:05 +0300

    Tests refactoring

---
+++ src/UnitTests/Usage/ApiClientInvokerBehavior.stuff.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using MyLab.ApiClient.Contracts.Attributes.ForMethod;
+using MyLab.ApiClient.Contracts.Attributes.ForParameters;
+
+namespace UnitTests.Usage;
+
+public partial class ApiClientInvokerBehavior
+{
+    public static object?[][] GetUnexpectedTestCases()
+    {
+        return new object?[][]
+        {
+            [
+                new HttpResponseMessage(HttpStatusCode.BadRequest),
+                true
+            ],
+            [
+                new HttpResponseMessage(HttpStatusCode.NotFound),
+                false
+            ]
+        };
+    }
+
+    public static object?[][] GetHandlersCases()
+    {
+        var successStringResponse = new HttpResponseMessage(HttpStatusCode.OK);
+        successStringResponse.Content = new StringContent("foo");
+
+        return new object?[][]
+        {
+            [
+                successStringResponse,
+                "foo",
+                HttpStatusCode.OK
+            ],
+            [
+                new HttpResponseMessage(HttpStatusCode.NotFound),
+                null,
+                HttpStatusCode.NotFound
+            ]
+        };
+    }
+
+    interface IContract
+    {
+        [Get("get")]
+        Task GetAsync();
+
+        [Post("echo")]
+        Task<string> EchoAsync([StringContent] string value);
+    }
+}
